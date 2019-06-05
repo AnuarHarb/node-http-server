a56751a959a0b9de23d464a404c50de524a8c938
Anuar Harb <anuarharb@hotmail.com>
2019-06-04 20:47:54 -0500

    fs.access para saber si un archivo existe

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 const port = 8000;
+
 
 const server = http.createServer(manejadorDeRespuesta);
 
 function manejadorDeRespuesta(req, res){
-  res.write('Hola desde el servidor!');
-  res.end();
+  fs.access('static/index.html', fs.constants.F_OK, function(err) {
+    if(err) {
+      res.write('El archivo no existe');
+      res.end();
+    } else {
+      res.write('El archivo si exite');
+      res.end();
+    }
+  })
 }
 
 server.listen(port, function(){
